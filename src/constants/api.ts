987e6817c45aa98cb5eb881fd922548f75395171
MD Maruf Hasan <marufhasan.cse@gmail.com>
2026-05-14 09:23:04 +0600

    updated

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,3 @@
 // src/constants/api.ts
-export const BASE_URL = "http://10.10.20.30:7000/api/v1";
+export const BASE_URL = "http://10.10.20.30:7007/api/v1";
 export const SOCKET_URL = BASE_URL.replace(/\/api\/v1$/, "");

262a2388b785c586e1e1f7b6c936d5e3c02498f4
Khaled Siddique <khaledssbd@gmail.com>
2026-05-21 15:17:43 +0600

    updated

--- src/services/orderApi.ts
+++ src/services/orderApi.ts
@@ -64,6 +64,16 @@ export type Order = {
     insuredVehicle?: boolean;
     topRated?: boolean;
   };
+  myRating?: {
+    _id?: string;
+    order?: string;
+    customer?: string;
+    driver?: string;
+    rating?: number;
+    feedback?: string;
+    createdAt?: string;
+    updatedAt?: string;
+  } | null;
   total: number;
   bagCountAtPickup?: number;
   bagCountAtDelivery?: number;
